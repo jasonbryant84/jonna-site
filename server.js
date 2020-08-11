7a0885e665e0547d1a6e864af4173276c4312038
Jason Bryant <jasonbryant@invisionapp.com>
2020-08-11 12:40:24 +0100

    add server.js and update Procfile

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const { createServer } = require('http'),
+    { parse } = require('url'),
+    next = require('next'),
+    port = process.env.PORT || 3000,
+    dev = process.env.NODE_ENV !== 'production',
+    app = next({ dev }),
+    handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(3000, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
